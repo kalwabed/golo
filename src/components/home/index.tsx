0ff897ae18a876142c94d276b837cb8372aed260
kalwabed <49640654+kalwabed@users.noreply.github.com>
2022-04-04 11:36:38 +0700

    feat: card in user list

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -16,16 +16,32 @@ const HomePageRenderer = () => {
 
   return (
     <div className="flex flex-col">
-      <h1 className="text-4xl mb-4">User list</h1>
+      <h1 className="text-4xl mb-4">User List</h1>
 
       <div className="grid grid-cols-2 md:grid-cols-4 gap-6">
+        {!data?.results &&
+          [1, 2, 3, 4].map(i => (
+            // loading skeleton
+            <div key={i} className="bg-yellow-500 p-4 rounded h-20 animate-pulse flex items-center space-x-2">
+              <span className="w-10 h-10 bg-yellow-600 animate-pulse rounded-full"></span>
+              <span className="w-2/3 h-6 bg-yellow-600 animate-pulse rounded"></span>
+            </div>
+          ))}
+
         {data?.results?.map(user => (
           <Link
             to={`/users/${user.login.uuid}`}
             key={user.email}
-            className="p-4 border border-gray-500 rounded hover:bg-yellow-500 transition"
+            className="p-4 border border-yellow-700 rounded bg-yellow-500 transition hover:-translate-y-2 hover:shadow-lg hover:bg-yellow-300 transform flex items-center space-x-2"
           >
-            <p>
+            <img
+              src={user.picture.thumbnail}
+              alt={`${user.name.first} picture`}
+              loading="lazy"
+              decoding="async"
+              className="rounded-full"
+            />
+            <p className="font-semibold">
               {user.name.first} {user.name.last}
             </p>
           </Link>
